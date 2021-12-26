b30b8a294498ce765698dd40df4a750f8debe689
Daniel Eklöf <daniel@ekloef.se>
2021-12-26 15:00:13 +0100

    put_char fixup

--- terminal.c
+++ terminal.c
@@ -3506,6 +3506,7 @@ print_spacer(struct terminal *term, int col, int remaining)
  *  - update the cursor
  *  - linewrap
  *  - erase sixels
+ *  - erase URIs (but it _does_ emit them if one is active)
  *
  * Limitiations:
  *   - double width characters not supported
