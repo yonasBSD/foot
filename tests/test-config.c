c0aa4144048091168fd43fb279893a8d224432a9
Daniel Eklöf <daniel@ekloef.se>
2021-12-30 21:49:08 +0100

    test: config: colors: test ‘256’ is an invalid key

--- tests/test-config.c
+++ tests/test-config.c
@@ -623,6 +623,8 @@ test_section_colors(void)
                    &conf.colors.table[i]);
     }
 
+    test_invalid_key(&ctx, &parse_section_colors, "256");
+
     /* TODO: alpha (float in range 0-1, converted to uint16_t) */
     /* TODO: jump-labels (two colors) */
     /* TODO: scrollback-indicator (two colors) */
