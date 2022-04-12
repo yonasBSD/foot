a1796ba5cd1cc7b6ef03021d7db57503e445b5dd
Daniel Eklöf <daniel@ekloef.se>
2022-04-12 15:23:41 +0200

    pgo: sync up stub version of slave_spawn()

--- pgo/pgo.c
+++ pgo/pgo.c
@@ -104,8 +104,8 @@ spawn(struct reaper *reaper, const char *cwd, char *const argv[],
 
 pid_t
 slave_spawn(
-    int ptmx, int argc, const char *cwd, char *const *argv, const char *term_env,
-    const char *conf_shell, bool login_shell,
+    int ptmx, int argc, const char *cwd, char *const *argv, char *const *envp,
+    const char *term_env, const char *conf_shell, bool login_shell,
     const user_notifications_t *notifications)
 {
     return 0;
