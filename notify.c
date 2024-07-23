d5c773a58b2cdadf7aee2fe16664c168e750b953
Daniel Eklöf <daniel@ekloef.se>
2024-07-23 18:48:45 +0200

    notify: bug: always adjust amount of data left in stdout buffer

--- notify.c
+++ notify.c
@@ -66,10 +66,10 @@ consume_stdout(struct notification *notif, bool eof)
         left -= len + (eol != NULL ? 1 : 0);
     }
 
-    if (left > 0) {
+    if (left > 0)
         memmove(notif->stdout_data, data, left);
-        notif->stdout_sz = left;
-    }
+
+    notif->stdout_sz = left;
 }
 
 static bool
